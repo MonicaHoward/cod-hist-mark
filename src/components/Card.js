64b8e4566bb37c1959eeeadc11637ff8f625c649
Monica Howard <mo@Monicas-Air.attlocal.net>
2020-12-06 14:21:06 -0600

    updates Card UI

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,14 +2,22 @@ import React from 'react';
 
 const Card = ({key, title, address, moreInfo, type, yearEstablished, markerText}) => {
     return(
-        <div>
-            <p>{title}</p>
-            <p>{address}</p>
-            <p>{moreInfo}</p>
-            <p>{type}</p>
-            <p>{yearEstablished}</p>
+        <div className="fl w-100 w-25-ns pa2">
+            <div class="outline bg-white pv4">
+            <article className=" ">
+                <div className="tc">
+                    <p className="ttu">{type}</p>
+                    <h1 className="f5 f4-ns fw6 mid-gray">{title}</h1>
+                    <h2 className="f6 gray fw2 ttu tracked">{address}</h2>
+                    <hr className="mw3 bb bw1 b--black-10"/>
+                </div>
+                <p className="lh-copy measure center f6 black-70">{moreInfo}</p>
+                <h5>Year Established: {yearEstablished}</h5>
+            </article>
 
+            </div>
         </div>
+        
     )
 }
 export default Card;
--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,22 +3,24 @@ import Card from './Card'
 
 const CardList = ({data}) => {
     return(
-        <div>
-        {
-            data.map((marker, k) => {
-                return(
-                    <Card 
-                        key={k}
-                        title={data[k].title}
-                        address={data[k].address}
-                        moreInfo={data[k].marker_text}
-                        type={data[k].classification}
-                        yearEstablished={data[k].year}
-                       
-                    />
-                )
-            })
-        }
+        <div class="mw9 center ph3-ns">
+            <div class="cf ph2-ns">
+            {
+                data.map((marker, k) => {
+                    return(
+                        <Card 
+                            key={k}
+                            title={data[k].title}
+                            address={data[k].address}
+                            moreInfo={data[k].marker_text}
+                            type={data[k].classification}
+                            yearEstablished={data[k].year}
+                        
+                        />
+                    )
+                })
+            }
+            </div>
         </div>
     ) 
 }
